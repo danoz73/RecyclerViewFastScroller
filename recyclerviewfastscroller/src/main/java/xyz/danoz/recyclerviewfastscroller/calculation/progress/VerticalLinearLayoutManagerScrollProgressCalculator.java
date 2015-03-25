8f9d256780e7c55e95ac9c66b429a0a82dd388ae
Maxr1998 <Maxr1998@users.noreply.github.com>
2015-03-25 20:15:23 +0100

    Update VerticalLinearLayoutManagerScrollProgressCalculator.java
    Fix braces

--- recyclerviewfastscroller/src/main/java/xyz/danoz/recyclerviewfastscroller/calculation/progress/VerticalLinearLayoutManagerScrollProgressCalculator.java
+++ recyclerviewfastscroller/src/main/java/xyz/danoz/recyclerviewfastscroller/calculation/progress/VerticalLinearLayoutManagerScrollProgressCalculator.java
@@ -26,8 +26,9 @@ public class VerticalLinearLayoutManagerScrollProgressCalculator extends Vertica
         int lastFullyVisiblePosition = layoutManager.findLastCompletelyVisibleItemPosition();
 
         View visibleChild = recyclerView.getChildAt(0);
-        if (visibleChild == null)
+        if (visibleChild == null) {
             return 0;
+        }
         ViewHolder holder = recyclerView.getChildViewHolder(visibleChild);
         int itemHeight = holder.itemView.getHeight();
         int recyclerHeight = recyclerView.getHeight();
